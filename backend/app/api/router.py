4d92574f8a2cffd20cdfd61a5c03a05280f7dc74
Dmitry <dm1398@yandex.ru>
2026-05-01 20:35:30 +0300

    Add ETL router to API endpoint

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -2,7 +2,8 @@
 
 from fastapi import APIRouter
 
-from app.api.routers import health
+from app.api.routers import etl, health
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(health.router)
+api_router.include_router(etl.router)
