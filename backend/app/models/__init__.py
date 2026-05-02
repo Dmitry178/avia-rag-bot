ee6a8715e97736ed5182a4f462dfe056ffd14433
Dmitry <dm1398@yandex.ru>
2026-05-02 13:38:16 +0300

    Add chat and chat message models with role enumeration

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,14 @@
 """SQLModel table definitions."""
 
+from app.models.chat import Chat
+from app.models.chat_message import ChatMessage, MessageRole
 from app.models.chunk_meta import ChunkMeta
 from app.models.index_manifest import IndexManifest
 
-__all__ = ["ChunkMeta", "IndexManifest"]
+__all__ = [
+    "Chat",
+    "ChatMessage",
+    "ChunkMeta",
+    "IndexManifest",
+    "MessageRole"
+]
