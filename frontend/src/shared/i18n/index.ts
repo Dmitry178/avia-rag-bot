793c45cd530df40ef97c0569ead025b583de849c
Dmitry <dm1398@yandex.ru>
2026-05-03 22:08:36 +0300

    Add i18n module exports

---
+++ frontend/src/shared/i18n/index.ts
@@ -0,0 +1,3 @@
+export { DEFAULT_LOCALE, LOCALE_INTL_TAG, SUPPORTED_LOCALES, type Locale } from "./config";
+export { applyDocumentLocale, readStoredLocale, translate, type TranslationParams } from "./translate";
+export { useI18nStore, useTranslation } from "./store";
