109e63754428be62eb5b05bea2f6b8c024c376d9
Dmitry <dm1398@yandex.ru>
2026-05-10 20:19:29 +0300

    Export ChatType enum in models init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 """SQLModel table definitions."""
 
-from app.models.chat import Chat
+from app.models.chat import Chat, ChatType
 from app.models.chat_message import ChatMessage, MessageRole
 from app.models.chunk_meta import ChunkMeta
 from app.models.index_manifest import IndexManifest
@@ -8,6 +8,7 @@ from app.models.index_manifest import IndexManifest
 __all__ = [
     "Chat",
     "ChatMessage",
+    "ChatType",
     "ChunkMeta",
     "IndexManifest",
     "MessageRole"
