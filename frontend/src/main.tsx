a962e5f46e2c5c34c260816527dc9ddf7e1f12ab
Dmitry <dm1398@yandex.ru>
2026-05-10 16:47:07 +0300

    Remove unused dark theme import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,6 @@ import { applyDocumentLocale, readStoredLocale } from "@/shared/i18n";
 import { applyTheme, readStoredTheme } from "@/theme/applyTheme";
 
 import "primeicons/primeicons.css";
-import "primereact/resources/themes/lara-dark-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "@/styles/global.css";
 
