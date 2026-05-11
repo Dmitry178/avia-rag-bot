8a30569c0d3bb2c9a5e41417e53d346b0566e9d7
Dmitry <dm1398@yandex.ru>
2026-05-11 22:38:03 +0300

    Add RAG method help translation utilities

--- frontend/src/shared/i18n/index.ts
+++ frontend/src/shared/i18n/index.ts
@@ -1,3 +1,4 @@
+export { getRagMethodHelp, type RagMethodHelp } from "./ragMethods";
 export { DEFAULT_LOCALE, LOCALE_INTL_TAG, SUPPORTED_LOCALES, type Locale } from "./config";
 export { applyDocumentLocale, readStoredLocale, translate, type TranslationParams } from "./translate";
 export { useI18nStore, useTranslation } from "./store";
