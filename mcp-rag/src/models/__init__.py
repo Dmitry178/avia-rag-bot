557ccd77cda3157d0ae4be9915dd82cdbc38956f
Dmitry <dm1398@yandex.ru>
2026-08-12 21:51:25 +0300

    Add SQLModel table definitions for KB artifacts

---
+++ mcp-rag/src/models/__init__.py
@@ -0,0 +1,6 @@
+"""SQLModel table definitions for KB artifacts."""
+
+from src.models.chunk_meta import ChunkMeta
+from src.models.index_manifest import IndexManifest
+
+__all__ = ["ChunkMeta", "IndexManifest"]
